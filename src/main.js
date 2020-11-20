2ec26c9f4db5dae48d75748bc2090279741a28e5
Zahnen Garner <zahnen@gmail.com>
2020-11-20 09:23:52 -0800

    confirm user input values + import from exchange-service.js

--- src/main.js
+++ src/main.js
@@ -2,3 +2,17 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import Exchange from './js/exchange-service.js'; 
+
+$(document).ready(function() {
+  $('#exchangeForm').submit(function(event) {
+    event.preventDefault();
+    Exchange.convertUSD()
+      .then(function() {
+        let amount = $('#amount').val();
+        let usdTo = $("select#convertTo").val();
+        console.log(amount);
+        console.log(usdTo);
+      });
+  });
+});
